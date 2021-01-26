580d89b326fde819308e53951899144bb2e173cb
raja015 <40671459+raja015@users.noreply.github.com>
2021-01-26 22:08:21 +0530

    todo V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,6 +16,7 @@ import { TodoComponent } from './todo/todo.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule
     // MatSliderModule
   ],
   providers: [],
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
@@ -6,13 +7,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
-  TaskList=[];
+  TaskList:any[]=[];
+  displayForm:boolean=false;
+  displayUpdate:boolean=false;
+  displaySave:boolean=true;
+  invalidForm:boolean=true;
+  task:string;
+  taskDiscription:string;
 
 
+  currentIndex=null;
+  taskreflen:any;
 
-  add(listData:string){
-    this.TaskList.push(listData);
+  add(){
+    this.TaskList.push({
+      title:this.task,
+      discription:this.taskDiscription
+    });
+
+    this.task="";
+    this.taskDiscription="";
+
+
   }
+
+  formValidatiom(){
+    console.log(this.task);
+    console.log(this.invalidForm);
+    if(this.task.length>=3){
+      this.invalidForm=false;
+    }
+    else
+    this.invalidForm=true;
+
+    console.log(this.task);
+    console.log(this.invalidForm);
+  }
+
+  clear(i:number){
+    this.TaskList.splice(i,1);
+
+
+  }
+
+  edit(i:number){
+
+    this.task=this.TaskList[i].title;
+    this.taskDiscription=this.TaskList[i].discription;
+    this.currentIndex=i;
+    this.displayUpdate=true;
+    this.displaySave=false;
+  }
+
+  update(){
+    this.TaskList[this.currentIndex].title=this.task;
+    this.TaskList[this.currentIndex].discription=this.taskDiscription;
+    this.currentIndex=null;
+
+    this.displaySave=true;
+    this.displayUpdate=false;
+
+    this.task="";
+    this.taskDiscription=""
+  }
+
+  openForm(){
+    this.displayForm=true;
+  }
+
+  closeForm(){
+    this.displayForm=false;
+  }
+
 
   constructor() { }
   ngOnInit(): void {
